751ac51f57099e946ad23ad74f21cebfc233adea
Mark Weeber <m.webber@inbox.ru>
2025-06-23 17:40:24 +0600

    adding score board UI

---
+++ Assets/Scripts/UI/ScoreBoardElementUI.cs
@@ -0,0 +1,30 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ScoreBoardElementUI : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _numberText;
+    [SerializeField] private TMP_Text _playerNameText;
+    [SerializeField] private TMP_Text _playerLastScore;
+    [SerializeField] private TMP_Text _playerBestScore;
+
+    public void SetNumber(int number)
+    {
+        _numberText.text = number.ToString();
+    }
+
+    private void SetPlayerName(string name)
+    {
+        _playerNameText.text = name;
+    }
+
+    private void SetPlayerLastScore(int score)
+    {
+        _playerLastScore.text = score.ToString();
+    }
+
+    private void SetPlayerBestScore(int score)
+    {
+        _playerBestScore.text = score.ToString();
+    }
+}
